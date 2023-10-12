c0e16529d7873de35a4f16ac5ab8c50e40428a8f
PMehar <pujamehar05@gmail.com>
2023-10-12 18:42:04 +0530

    did setup using create-react app + configured tailwind

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-lg font-bold bg-green-600'>Hi my name is puja , Lets get started</h1>
+    </div>
+  );
+}
+
+export default App;
